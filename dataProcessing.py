2a5f2b96ca2c6e0fe88e2d3c7a98103835f6c88b
MingT-L8553 <1071252445@qq.com>
2021-02-20 19:29:52 +0800

    The purpose of this program is data cleaning Using four function to data processing function one: formatTxt() 	This function is to extract all the Chinese characters in the 	text function two: cutWords() 	This function is to count the words in the Chinese text function three: changeType() 	This function is under construction, The purpose is to convert 	the dictionary data into JSON data function four: wordCloud() 	This function is under construction, Using the statistical JSON 	data to generate word cloud

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,14 +2,58 @@
 # -*- coding:utf-8 -*-
 #@Time  : 2021/2/20 16:52
 #@Author: 李明特
-#@File  : formatTXT.py
+#@File  : dataProcessing.py
 
 
 import re
-txt = open("comments.txt", "r", encoding='utf-8').read()
-chinese_characters = re.compile(u'[\u4e00-\u9fa5]')
-res = re.findall(chinese_characters,txt,re.S)
-result = ''.join(res)
-with open('result_comments.txt','w') as f:
-    f.write(result)
+import json
+import jieba
+
+# 提取所有汉字
+def formatTxt():
+    sentences = list()
+    with open('comments.txt','r',errors='ignore')as f:
+        lines = f.readlines()
+        for sentence in lines:
+            res = re.findall('[\u4e00-\u9fa5]', sentence)
+            if len(res) != 0:
+                result = ''.join(res)
+            else:
+                continue
+            sentences.append(result)
+    with open('format_comments.txt','w') as f:
+        for s in sentences:
+            f.write(s)
+
+# 分词
+def cutWords():
+    # 用键值对的方式统计单词
+    count = {}
+    txt = open('format_comments.txt','r',errors='ignore').read()
+    words = jieba.lcut(txt)
+    # 获取停用词
+    stopwords = [i.strip() for i in open('stop_words.txt', encoding='utf-8').readlines()]
+    for word in words:
+        # 去除单词和停用词
+        if len(word) != 1 and word not in stopwords:
+            count[word] = count.get(word, 0) + 1
+        else:
+            continue
+
+# 保存为json数据
+def changeType():
+    pass
+
+
+    # items = list(counts.items())
+    # items.sort(key=lambda x: x[1], reverse=True)
+
+    # for i in range(100):
+    #     word, count = items[i]
+    #     print("{0:<5}{1:>5}".format(word, count))
+
+if __name__ == '__main__':
+    # formatTxt()
+    cutWords()
+
 
